bbe053081824f9c04b9a230dc8eeeb7a43ffd5d2
zhujian3 <zhujian3@myvuw.ac.nz>
2017-06-01 15:54:23 +1200

    t tatus,time# P#lease enter the commit message for your changes. Lines starting

--- Database/SQL_Draft_Ideas/mailDeliveryData.sql
+++ Database/SQL_Draft_Ideas/mailDeliveryData.sql
@@ -1,20 +1,28 @@
 DROP TABLE IF EXISTS mailDeliveryEvents;
 
--- TODO: EXTRA COLUMNS in mailDeliveryEvents: status (delivered or not), TIME STAMP (If delivered == true some random value, else 0 or NULL)
+-- TODO: EXTRA COLUMNS in mailDeliveryEvents: status (delivered or not), TIME STAMP (in days)
 -- TODO: Add more data.
 
 
 -- Create the table that will store the mail delivery events:
-CREATE TABLE mailDeliveryEvents (id SERIAL PRIMARY KEY, origin CHAR(20), destination CHAR(20),
-weight INT, volume INT, priority CHAR(20));
+CREATE TABLE mailDeliveryEvents (
+    id SERIAL PRIMARY KEY,
+    origin CHAR(20),
+    destination CHAR(20),
+    weight INT,
+    volume INT,
+    priority CHAR(20),
+    status CHAR(10),
+    time INT
+);
 
 -- Add data to mailDeliveryEvents: (some dummy values)
-INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority) VALUES ('Wellington','Auckland',12.01,34.02,'low');
-INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority) VALUES ('Wellington','Auckland',13.07,4.02,'low');
-INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority) VALUES ('Wellington','Auckland',15.1,3.02,'low');
-INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority) VALUES ('Christchurch','Wellington',10.1,3.01,'high');
-INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority) VALUES ('Taupo','Waikanai',7.1,9.03,'high');
-INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority) VALUES ('Wellington','Taupo',13.1,23.01,'low');
+INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority,status,time) VALUES ('Wellington','Auckland',12.01,34.02,'low');
+INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority,status,time) VALUES ('Wellington','Auckland',13.07,4.02,'low');
+INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority,status,time) VALUES ('Wellington','Auckland',15.1,3.02,'low');
+INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority,status,time) VALUES ('Christchurch','Wellington',10.1,3.01,'high');
+INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority,status,time) VALUES ('Taupo','Waikanai',7.1,9.03,'high');
+INSERT INTO mailDeliveryEvents (origin,destination,weight,volume,priority,status,time) VALUES ('Wellington','Taupo',13.1,23.01,'low');
 
 
 -- selecting data from mailDeliveryEvents, show all the rows from the table
